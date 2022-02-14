0e6f890aa2980d5fe62a9754a08de1baa29ed4ce
Sigrarr <wiktor.banaszewski@protonmail.com>
2022-02-14 12:22:33 +0100

    (add ConstantsAndUnits, aberration: more precise constant + stricter test cmp. with true value)

--- src/main/java/com/github/sigrarr/lunisolarcalc/space/AberrationEarthSunCalculator.java
+++ src/main/java/com/github/sigrarr/lunisolarcalc/space/AberrationEarthSunCalculator.java
@@ -1,10 +1,13 @@
 package com.github.sigrarr.lunisolarcalc.space;
 
 import com.github.sigrarr.lunisolarcalc.space.periodicterms.SunLongitudeVariationPeriodicTerms;
+import com.github.sigrarr.lunisolarcalc.util.ConstantsAndUnits;
 
 public final class AberrationEarthSunCalculator {
 
-    private static final double AU_LIGHT_TIME_DAYS = 0.005775518;
+    private static final double AU_LIGHT_TIME_DAYS = (
+        (double) ConstantsAndUnits.ASTRONOMICAL_UNIT_METERS / (double) ConstantsAndUnits.LIGHT_SPEED_METERS_PER_SECOND
+    ) / 3600.0 / 24.0;
     private SunLongitudeVariationPeriodicTerms periodicTerms = new SunLongitudeVariationPeriodicTerms(); 
     private EarthSunRadiusCalculator radiusCalculator = new EarthSunRadiusCalculator();
     
--- src/test/java/com/github/sigrarr/lunisolarcalc/space/AberrationCalculatorTest.java
+++ src/test/java/com/github/sigrarr/lunisolarcalc/space/AberrationCalculatorTest.java
@@ -2,6 +2,7 @@ package com.github.sigrarr.lunisolarcalc.space;
 
 import static org.junit.Assert.assertEquals;
 import static com.github.sigrarr.lunisolarcalc.util.MeanValueApproximations.SunEarthRelativeMotion.arcsecondsPerTimeMiliseconds;
+import static com.github.sigrarr.lunisolarcalc.util.MeanValueApproximations.SunEarthRelativeMotion.degreesPerTimeMiliseconds;
 
 import com.github.sigrarr.lunisolarcalc.time.Timeline;
 import com.github.sigrarr.lunisolarcalc.util.Calcs;
@@ -16,24 +17,25 @@ public class AberrationCalculatorTest {
     @Test
     public void shouldCalculateAberration() {
         // Meeus 1998, Example 25.a-b, pp. 165, 169
-        double tau = Timeline.julianDayToMillenialTau(2448908.5);
+        double jd = 2448908.5;
+        double cT = Timeline.julianDayToCenturialT(jd);
+        double tau = Timeline.julianDayToMillenialTau(jd);
         double actualAberration = calculator.calculateAberration(tau);
-        double precision = arcsecondsPerTimeMiliseconds(215);
 
         // Meeus 1998, Example 25.b, p. 169
         double aberrationCalculatedWithSimplerMethodArcseconds = -20.539;
-        assertEquals(aberrationCalculatedWithSimplerMethodArcseconds, Calcs.toArcseconds(Math.toDegrees(actualAberration)), precision);
+        assertEquals(aberrationCalculatedWithSimplerMethodArcseconds, Calcs.toArcseconds(Math.toDegrees(actualAberration)), arcsecondsPerTimeMiliseconds(215));
 
         // Meeus 1998, Example 25.a, p. 165
         double trueVSOP87GeometricLongitudeDegrees = 199.0 + Calcs.arcminutesToDegrees(54.0) + Calcs.arcsecondsToDegrees(26.18);
         double trueVSOP87ApparentLongitudeDegrees = 199.0 + Calcs.arcminutesToDegrees(54.0) + Calcs.arcsecondsToDegrees(21.56);
-        double deltaPsiDegrees = Math.toDegrees(deltaPsiCalculator.calculateNutuation(Timeline.millenialTauToCenturialT(tau)));
+        double deltaPsiDegrees = Math.toDegrees(deltaPsiCalculator.calculateNutuation(cT));
         double implicitAberrationDegrees = trueVSOP87ApparentLongitudeDegrees - trueVSOP87GeometricLongitudeDegrees - deltaPsiDegrees;
-        assertEquals(implicitAberrationDegrees, Math.toDegrees(actualAberration), Calcs.arcsecondsToDegrees(precision));
+        assertEquals(implicitAberrationDegrees, Math.toDegrees(actualAberration), degreesPerTimeMiliseconds(115));
 
         // Meeus 1998, Example 27.b, pp. 180-181
         tau = Timeline.julianDayToMillenialTau(2437837.38589);
         actualAberration = calculator.calculateAberration(tau);
-        assertEquals(-20.161, Calcs.toArcseconds(Math.toDegrees(actualAberration)), precision);
+        assertEquals(-20.161, Calcs.toArcseconds(Math.toDegrees(actualAberration)), arcsecondsPerTimeMiliseconds(215));
     }
 }
