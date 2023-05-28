123fa0fd6e6deb7b75e257547b771b30dab6cc7f
Sigrarr <wiktor.banaszewski@protonmail.com>
2023-05-28 18:45:11 +0200

    Util: add FlexPentadBuffer test and remove FlexTriadBuffer

---
+++ src/test/java/com/github/sigrarr/lunisolarcalc/util/FlexPentadBufferTest.java
@@ -0,0 +1,69 @@
+package com.github.sigrarr.lunisolarcalc.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FlexPentadBufferTest {
+
+    private FlexPentadBuffer<Integer> pentad;
+
+    @Test
+    public void shouldInitializeAndUpdate() {
+        pentad = new FlexPentadBuffer<Integer>();
+        assertStateAndGettersOutput(new Integer[5]);
+        pentad = new FlexPentadBuffer<Integer>(1, 2, 3, 4, 5);
+        assertStateAndGettersOutput(new Integer[] {1, 2, 3, 4, 5});
+        pentad.setBackBack(6);
+        assertStateAndGettersOutput(new Integer[] {6, 2, 3, 4, 5});
+        pentad.setBack(null);
+        assertStateAndGettersOutput(new Integer[] {6, null, 3, 4, 5});
+        pentad.setCenter(7);
+        assertStateAndGettersOutput(new Integer[] {6, null, 7, 4, 5});
+        pentad.setFront(8);
+        assertStateAndGettersOutput(new Integer[] {6, null, 7, 8, 5});
+        pentad.setFrontFront(9);
+        assertStateAndGettersOutput(new Integer[] {6, null, 7, 8, 9});
+        pentad.clear();
+        assertStateAndGettersOutput(new Integer[5]);
+    }
+
+    @Test
+    public void shouldCycle() {
+        pentad = new FlexPentadBuffer<Integer>(1, 2, 3, 4, 5);
+        pentad.push(6);
+        assertStateAndGettersOutput(new Integer[] {2, 3, 4, 5, 6});
+        pentad.push(null);
+        assertStateAndGettersOutput(new Integer[] {3, 4, 5, 6, null});
+        pentad.pushFromBack(7);
+        assertStateAndGettersOutput(new Integer[] {7, 3, 4, 5, 6});
+        pentad.pushFromBack(null);
+        assertStateAndGettersOutput(new Integer[] {null, 7, 3, 4, 5});
+    }
+
+    private void assertStateAndGettersOutput(Integer[] expectedElements) {
+        assertEquals(expectedElements[0], pentad.getBackBack());
+        assertEquals(expectedElements[1], pentad.getBack());
+        assertEquals(expectedElements[2], pentad.getCenter());
+        assertEquals(expectedElements[3], pentad.getFront());
+        assertEquals(expectedElements[4], pentad.getFrontFront());
+
+        assertEquals(expectedElements[0] != null, pentad.hasBackBack());
+        assertEquals(expectedElements[1] != null, pentad.hasBack());
+        assertEquals(expectedElements[2] != null, pentad.hasCenter());
+        assertEquals(expectedElements[3] != null, pentad.hasFront());
+        assertEquals(expectedElements[4] != null, pentad.hasFrontFront());
+
+        boolean allEmpty = true;
+        boolean allPresent = true;
+        for (int i = 0; i < 5; i++) {
+            assertEquals(expectedElements[i], pentad.get(i));
+            if (expectedElements[i] == null)
+                allPresent = false;
+            else
+                allEmpty = false;
+        }
+        assertEquals(allPresent, pentad.isComplete());
+        assertEquals(allEmpty, pentad.isEmpty());
+    }
+}
